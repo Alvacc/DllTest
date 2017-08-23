bb71b4c499abeb8242f8ae4e96652b2f27ce0df1
Alvacc <409541016@qq.com>
2017-08-24 00:05:46 +0800

    Add files via upload

---
+++ UsingDll/UsingDll/Main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main(){
+	cout << "main" << endl;
+
+	typedef int(*FUNC)();
+	HMODULE hDll = LoadLibrary("DllTest.dll");
+	FUNC Main_Hello_Ma = (FUNC)GetProcAddress(hDll, "Hello_Wang");
+	Main_Hello_Ma();
+	return 0;
+}
