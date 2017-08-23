f4638651982d5c46d93e99bb2ad9fd213cf083e2
Alvacc <409541016@qq.com>
2017-08-23 19:59:40 +0800

    1st commit

---
+++ DllTest/DllTest/Hello_Ma.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int Hello_Ma() {
+
+	cout << "Hello, this is Ma" << endl;
+	return 0;
+}
---
+++ DllTest/DllTest/test.cpp
@@ -0,0 +1,7 @@
+#include "Hello_Ma.h"
+
+int main() {
+
+	Hello_Ma();
+	return 0;
+}
